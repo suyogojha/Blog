e341a80d39633071abb3ab7870e8fc8cb614abfc
Suyog Ojha <65763177+suyogojha@users.noreply.github.com>
2024-09-23 12:04:38 +0100

    Update settings.py

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4b5=kb%i^!ujji594f%cw6a=%5o#oq_dq1ue&zhzwkh=hr6jkv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
